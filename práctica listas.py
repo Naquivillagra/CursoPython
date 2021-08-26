4e159226e0de458e9080b3ba26bc2ba4f2754311
Naquivillagra <89555949+Naquivillagra@users.noreply.github.com>
2021-08-25 21:04:00 -0300

    Add files via upload

---
+++ práctica listas.py
@@ -0,0 +1,18 @@
+## milista=['María','Pepe', 'Marta', 'Antonio']
+milista=["María", 5, True, 78.35]*3
+
+## milista.append("Sandra") #append agrega los elementos al final de la lista
+## milista.insert(2,"Sandra") #insert agrega el elemento donde lo indiquemos
+
+## milista.extend(["Sandra", "Ana", "Lucía"]) # para agregar varios elementos a la lista
+## milista.remove("Ana") #Elimina un elemento de la lista
+
+## milista.pop() # elimina el último elemento de la lista
+## print(milista.index("Antonio")) # .index se usa para preguntar en qué índice se encuentra el elemento
+## print("Pepe" in milista)  #pregunta si el elemento se encuentra en la lista
+
+## print(milista[:]) #para imprimir lista completa
+
+## milista2=["Sandra", "Lucía"]
+## milista3=milista+milista2
+print(milista[:])
